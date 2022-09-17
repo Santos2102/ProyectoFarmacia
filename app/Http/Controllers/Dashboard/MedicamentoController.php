44ed4b88e8c0fb1ec67a606ee19662a668a38332
Santos2102 <mikesan2102@umes.edu.gt>
2022-09-16 21:59:30 -0600

    Creando controlador para las tablas de medicamento, miscelaneo y usuario

---
+++ app/Http/Controllers/Dashboard/MedicamentoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Medicamento;
+use Illuminate\Http\Request;
+
+class MedicamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Medicamento  $medicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Medicamento $medicamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Medicamento  $medicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Medicamento $medicamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Medicamento  $medicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Medicamento $medicamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Medicamento  $medicamento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Medicamento $medicamento)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Dashboard/MiscelaneoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Miscelaneo;
+use Illuminate\Http\Request;
+
+class MiscelaneoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Miscelaneo  $miscelaneo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Miscelaneo $miscelaneo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Miscelaneo  $miscelaneo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Miscelaneo $miscelaneo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Miscelaneo  $miscelaneo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Miscelaneo $miscelaneo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Miscelaneo  $miscelaneo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Miscelaneo $miscelaneo)
+    {
+        //
+    }
+}

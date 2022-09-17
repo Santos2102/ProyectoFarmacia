821601ff832c09ebe9b95c411fb21dad96929dad
Santos2102 <mikesan2102@umes.edu.gt>
2022-09-16 18:21:45 -0600

    Creando la base de datos proyectofarmacia y la tabla de medicamento

---
+++ database/migrations/2022_09_17_001222_create_medicamento_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicamento', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre",255);
+            $table->text("descripcion",255);
+            $table->string("laboratorio",255);
+            $table->string("presentacion",255);
+            $table->float("precio_unidad");
+            $table->string("imagen");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicamento');
+    }
+};

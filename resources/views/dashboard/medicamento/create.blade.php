a4d837523031b916018cdb7fca88c4a62c22c0f0
DouAnd-Cutz <110060761+DouAnd-Cutz@users.noreply.github.com>
2022-09-18 00:15:09 -0600

    centrando componentes

--- resources/views/dashboard/medicamento/create.blade.php
+++ resources/views/dashboard/medicamento/create.blade.php
@@ -30,55 +30,51 @@
 
     <img class="ima" src="/Assets/Image/logo.png" alt="" width="25%">
    
-    
-    <form action="{{route('medicamento.store')}}" method="post">
-        @include('dashboard.partials.sesion-flash-status')
-        <div class="row">
+    <div class="container">
+        <form action="{{route('medicamento.store')}}" method="post">
+            @include('dashboard.partials.sesion-flash-status')
 
-            <div class="col">
-                <label class="sub" ><b>Nombre:</b></label><br>
-                <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Nombre de Medicamento" aria-label="Disabled input example"  rows="1" type="text" name="nombre" value ="{{old('nombre')}}"> <br>
-                 @error('nombre')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
+        
+            <label class="sub" ><b>Nombre:</b></label><br>
+            <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Nombre de Medicamento" aria-label="Disabled input example"  rows="1" type="text" name="nombre" value ="{{old('nombre')}}"> <br>
+             @error('nombre')
+                <small class="text-danger">{{$message}}</small>
+             @enderror
 
-                <label for=""><b>Descripción:</b></label>
-                <textarea class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Descripción" aria-label="Disabled input example"  rows="3" name="descripcion">{{old('descripcion')}}</textarea><br>
-                @error('descripcion')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
+            <label for=""><b>Descripción:</b></label>
+            <textarea class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Descripción" aria-label="Disabled input example"  rows="3" name="descripcion">{{old('descripcion')}}</textarea><br>
+            @error('descripcion')
+                <small class="text-danger">{{$message}}</small>
+            @enderror
                 
-                <label class="sub" ><b>Laboratorio:</b></label><br>
-                <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Laboratorio" aria-label="Disabled input example"  rows="1" type="text" name="laboratorio" value ="{{old('laboratorio')}}"><br>
-                @error('laboratorio')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
+            <label class="sub" ><b>Laboratorio:</b></label><br>
+            <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Laboratorio" aria-label="Disabled input example"  rows="1" type="text" name="laboratorio" value ="{{old('laboratorio')}}"><br>
+            @error('laboratorio')
+                <small class="text-danger">{{$message}}</small>
+            @enderror
                 
-                <label class="sub" ><b>Presentación:</b></label><br>
-                <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese tipo de presentación" aria-label="Disabled input example"  rows="1" type="text" name="presentacion" value ="{{old('presentacion')}}"><br>
-                @error('presentacion')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror
+            <label class="sub" ><b>Presentación:</b></label><br>
+            <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese tipo de presentación" aria-label="Disabled input example"  rows="1" type="text" name="presentacion" value ="{{old('presentacion')}}"><br>
+            @error('presentacion')
+                <small class="text-danger">{{$message}}</small>
+            @enderror
                 
-                <label class="sub" ><b>Precio Unidad:</b></label><br>
-                <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Precio por unidad " aria-label="Disabled input example"  rows="1" type="text" name="precio_unidad" value ="{{old('precio_unidad')}}"><br>
-                @error('precio_unidad')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror 
+            <label class="sub" ><b>Precio Unidad:</b></label><br>
+            <input class="fo" id="exampleFormControlTextarea1" placeholder="Ingrese Precio por unidad " aria-label="Disabled input example"  rows="1" type="text" name="precio_unidad" value ="{{old('precio_unidad')}}"><br>
+            @error('precio_unidad')
+                <small class="text-danger">{{$message}}</small>
+            @enderror 
 
-                <label class="sub" ><b>Imagen:</b></label><br>
-                <input type="file" name="imagen" value ="{{old('imagen')}}"><br>
-                @error('imagen')
-                    <small class="text-danger">{{$message}}</small>
-                @enderror 
+            <label class="sub" ><b>Imagen:</b></label><br>
+            <input type="file" name="imagen" value ="{{old('imagen')}}"><br>
+            @error('imagen')
+                <small class="text-danger">{{$message}}</small>
+            @enderror 
 
 
-                <button id="but"  class="btn btn-warning" type="submit">Enviar</button>
-            </div>
+            <button id="but"  class="btn btn-warning" type="submit">Enviar</button>
 
-        </div>
-
-    </form>    
-      
+        </form>    
+    </div>
 </body>
 </html>
